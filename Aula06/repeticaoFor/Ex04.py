9fd835b149ccfe4eb881cb0326e247c50ad3bb35
RikelmySander <rikelmysander@gmail.com>
2024-09-22 15:50:19 -0300

    Exercicio Resolvido

--- Aula06/repeticaoFor/Ex04.py
+++ Aula06/repeticaoFor/Ex04.py
@@ -2,4 +2,13 @@
 Desenvolva um programa que faz uma contagem regressiva,
 imprimindo os números de 20 até 1.
 """
-
+import time 
+i = 20 
+while True:
+    if i >= 0:
+        time.sleep(1)
+        print(i)
+        i -=1
+        continue
+    else:
+        break
